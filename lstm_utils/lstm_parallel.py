updated linear trend computation, rolling mean figures, lstm projection can now take as input a prediction frequency and loss values are saved to csvs, fixed issue with loss plots

lstm_utils/lstm_parallel.py
@@ -57,7 +57,8 @@ def project(mega_df,
             look_back,
             num_prediction,
             n_features,
-            model):
+            model,
+            freq):
 
 
     ##original dimesion is time x transect
@@ -76,7 +77,15 @@ def project(mega_df,
     #prediction = scaler.inverse_transform(prediction)
 
     last_date = mega_df.index.values[-1]
-    prediction_dates = pd.date_range(last_date, periods=num_prediction+1, freq='31D').tolist()
+    if freq=='monthly':
+        frequency='31D'
+    elif freq=='seasonally':
+        frequency='91D'
+    elif freq=='biannually':
+        frequency='182D'
+    elif freq=='yearly':
+        frequency='365D'
+    prediction_dates = pd.date_range(last_date, periods=num_prediction+1, freq=frequency).tolist()
         
 
     forecast = prediction
@@ -155,7 +164,8 @@ def get_csvs(folder,
     return csvs
 
 def get_mega_df(csv_paths,
-                base):
+                base,
+                freq):
     name = os.path.splitext(os.path.basename(csv_paths[0]))[0]
     idx = name.find('_')
     num = name[idx+1:]
@@ -168,7 +178,14 @@ def get_mega_df(csv_paths,
     new_df = new_df.set_index(['datetime'])
     new_df = new_df.dropna()
     y_rolling = new_df.rolling('365D', min_periods=1).mean()
-    y1 = y_rolling.resample('31D').ffill()
+    if freq=='monthly':
+        y1 = y_rolling.resample('31D').ffill()
+    elif freq=='seasonally':
+        y1 = y_rolling.resample('91D').ffill()
+    elif freq=='biannually':
+        y1 = y_rolling.resample('182D').ffill()
+    elif freq=='yearly':
+        y1 = y_rolling.resample('365D').ffill()
     y1 = y1.dropna()
     df=y1
 
@@ -190,7 +207,14 @@ def get_mega_df(csv_paths,
         new_df = new_df.set_index(['datetime'])
         new_df = new_df.dropna()
         y_rolling = new_df.rolling('365D', min_periods=1).mean()
-        y1 = y_rolling.resample('31D').ffill()
+        if freq=='monthly':
+            y1 = y_rolling.resample('31D').ffill()
+        elif freq=='seasonally':
+            y1 = y_rolling.resample('91D').ffill()
+        elif freq=='biannually':
+            y1 = y_rolling.resample('182D').ffill()
+        elif freq=='yearly':
+            y1 = y_rolling.resample('365D').ffill()
         y1 = y1.dropna()
         df=y1
 
@@ -202,14 +226,16 @@ def get_mega_df(csv_paths,
 def make_mega_df(folder,
                  basename,
                  start_idx,
-                 end_idx):
+                 end_idx,
+                 freq):
     mega_df_save = os.path.join(folder, basename+'_'+str(start_idx)+'to'+str(end_idx)+'.csv')
     csv_list = get_csvs(folder,
                         basename,
                         start_idx,
                         end_idx)
     mega_df = get_mega_df(csv_list,
-                          basename)
+                          basename,
+                          freq)
     mega_df.to_csv(mega_df_save)
     return mega_df
 
@@ -223,7 +249,8 @@ def process_results(sitename,
                     observed_dates,
                     date_predict,
                     forecast_dates,
-                    mega_arr_forecast):
+                    mega_arr_forecast,
+                    lookback):
     n_features = np.shape(mega_arr_pred)[1]
     bootstrap = np.shape(mega_arr_pred)[2]
     idx = start_idx
@@ -302,7 +329,8 @@ def main(transect_folder,
          units=80,
          batch_size=32,
          lookback=3,
-         split_percent=0.80):
+         split_percent=0.80,
+         freq='monthly'):
     
     look_back=lookback
     num_prediction=num_prediction
@@ -313,17 +341,19 @@ def main(transect_folder,
     mega_df = make_mega_df(transect_folder,
                            sitename,
                            start_idx,
-                           end_idx)
+                           end_idx,
+                           freq)
     observed_dates = mega_df.index
     dataset, train_generator, test_generator, n_features, prediction_generator = setup_data(mega_df,
-                                                                                                  start_idx,
-                                                                                                  end_idx,
-                                                                                                  split_percent,
-                                                                                                  look_back,
-                                                                                                  batch_size)
+                                                                                            start_idx,
+                                                                                            end_idx,
+                                                                                            split_percent,
+                                                                                            look_back,
+                                                                                            batch_size)
     date_predict = observed_dates[lookback:]
     mega_arr_pred = np.zeros((len(date_predict), n_features, bootstrap))
     mega_arr_forecast = np.zeros((num_prediction+1, n_features, bootstrap))
+    histories = [None]*bootstrap
     for i in range(bootstrap):
         reset_keras()
         model, history = train_model(train_generator,
@@ -332,10 +362,8 @@ def main(transect_folder,
                             look_back,
                             units,
                             n_features)
-        plot_history(history)
-        if i == bootstrap-1:
-            plt.savefig(os.path.join(output_folder, 'loss_plot.png'), dpi=300)
-            plt.close()
+        histories[i] = history
+
         prediction = predict_data(model, prediction_generator)
         mega_arr_pred[:,:,i] = prediction
         forecast, forecast_dates = project(mega_df,
@@ -343,8 +371,11 @@ def main(transect_folder,
                                            look_back,
                                            num_prediction,
                                            n_features,
-                                           model)
+                                           model,
+                                           freq)
         mega_arr_forecast[:,:,i] = forecast
+        
+
 
     process_results(sitename,
                     output_folder,
@@ -355,8 +386,19 @@ def main(transect_folder,
                     observed_dates,
                     date_predict,
                     forecast_dates,
-                    mega_arr_forecast)
-    
+                    mega_arr_forecast,
+                    lookback)
+    i = 0
+    for history in histories:
+        # convert the history.history dict to a pandas DataFrame:     
+        hist_df = pd.DataFrame(history.history) 
+        # save to csv: 
+        hist_csv_file = os.path.join(output_folder,'history'+str(i)+'.csv')
+        hist_df.to_csv(hist_csv_file, index=False)
+        plot_history(history)
+        i=i+1
+    plt.savefig(os.path.join(output_folder, 'loss_plot.png'), dpi=300)
+    plt.close('all')
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -373,6 +415,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size",type=int, required=True, help="training batch size")
     parser.add_argument("--lookback",type=int, required=True, help="look back value")
     parser.add_argument("--split_percent",type=float, required=True, help="training data fraction")
+    parser.add_argument("--freq", type=str, required=True, help="prediction frequency (monthly, seasonally, biannually, yearly")
     args = parser.parse_args()
     main(args.transect_folder,
          args.output_folder,
@@ -385,7 +428,8 @@ if __name__ == "__main__":
          units=args.units,
          batch_size=args.batch_size,
          lookback=args.lookback,
-         split_percent=args.split_percent)   
+         split_percent=args.split_percent,
+         freq=args.freq)   
 
 
 

utils/getting_linear_trends.py
@@ -11,7 +11,16 @@ from scipy import stats
 plt.rcParams["figure.figsize"] = (16,6)
 
 def plot_timeseries_with_fit(data, min_year_filter = 1984, max_year_filter = 2023):
-
+    folder = os.path.dirname(data)
+    lt_folder = os.path.join(folder, 'linear_trends')
+    try:
+        os.mkdir(lt_folder)
+    except:
+        pass
+    name = os.path.basename(data)
+    name = os.path.splitext(name)[0]
+    new_name = name+'linear_trend_yearly.png'
+    fig_path = os.path.join(lt_folder, new_name)
     df = pd.read_csv(data)
     df.reset_index()
     df = df.dropna()
@@ -26,7 +35,7 @@ def plot_timeseries_with_fit(data, min_year_filter = 1984, max_year_filter = 202
     filter_df = df[df['datetimes']>datetime.datetime(min_year_filter-1,12,31)]
     filter_df = df[df['datetimes']<datetime.datetime(max_year_filter+1,1,1)]
     filter_df.reset_index()
-    
+
     filtered_datetimes = np.array(filter_df['datetimes'])
     shore_pos = np.array(filter_df['distances'])
     datetimes_seconds = [None]*len(filtered_datetimes)
@@ -38,12 +47,43 @@ def plot_timeseries_with_fit(data, min_year_filter = 1984, max_year_filter = 202
         datetimes_seconds[i] = dt_sec
     datetimes_seconds = np.array(datetimes_seconds)
     datetimes_years = datetimes_seconds/(60*60*24*365)
+
+
     x = datetimes_years
     y = shore_pos
-    result1 = stats.linregress(x,y)
+    
+    new_df = pd.DataFrame({'shoreline':list(y)},
+                          index=list(datetimes))
+    y2 = new_df.rolling('365D', min_periods=1).mean()
+    y2 = np.array(y2.shoreline)
+    result1 = stats.linregress(x,y2)
+    intercept1 = result1.intercept
     slope1 = result1.slope
+    r_value1 = result1.rvalue**2
+    intercept_err = result1.intercept_stderr
+    slope_err = result1.stderr
+    lab = ('OLS\nSlope: ' +
+          str(np.round(slope1,decimals=3)) + ' $+/-$ ' + str(np.round(slope_err, decimals=3)) +
+          '\nIntercept: ' +
+          str(np.round(intercept1,decimals=3)) + ' $+/-$ ' + str(np.round(intercept_err, decimals=3)) +
+          '\n$R^2$: ' + str(np.round(r_value1,decimals=3)))
+    fit1x = datetimes_years
+    fit1y = slope1*fit1x + intercept1
+    
+    plt.plot(datetimes, y2, color='navy', label='Yearly Moving Average')
+    plt.plot(datetimes, fit1y, '--', color='red', label=lab)
+    plt.minorticks_on()
+    plt.xticks(rotation=90)
+    plt.xlabel('Time (UTC)')
+    plt.ylabel('Cross-Shore Position (m)')
+    plt.xlim(min(datetimes), max(datetimes))
+    plt.ylim(min(y2), max(y2))
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(fig_path,dpi=300)
+    plt.close()
 
-    return slope1
+    return slope1, fig_path
 
 def gb(x1, y1, x2, y2):
     angle = degrees(atan2(y2 - y1, x2 - x1))
@@ -78,15 +118,18 @@ def batch_transect_timeseries(sitename,
     transects = gpd.read_file(transects_path)
     transects = transects.reset_index()
     slopes = [None]*len(transects)
+    fig_paths = [None]*len(transects)
     csvs = [None]*len(transects)
     for i in range(len(transects)):
         print(np.round(i/len(transects),2))
         csv = os.path.join(transects_csv_folder, sitename+str(i)+'.csv')
-        slope = plot_timeseries_with_fit(csv, min_year_filter=min_year_filter,max_year_filter=max_year_filter)
+        slope, fig_path = plot_timeseries_with_fit(csv, min_year_filter=min_year_filter,max_year_filter=max_year_filter)
         slopes[i] = slope
+        fig_paths[i] = fig_path
 
         
     transects['yearly_trend'] = slopes
+    transects['fig_paths'] = fig_paths
     max_slope = np.max(np.abs(slopes))
     scaled_slopes = (np.array(slopes)/1)*100
     new_lines = [None]*len(transects)
@@ -104,7 +147,8 @@ def batch_transect_timeseries(sitename,
         line = arr_to_LineString(line_arr)
         new_lines[i] = line
     
-    new_df = pd.DataFrame({'year_tr':slopes})
+    new_df = pd.DataFrame({'year_tr':slopes,
+                           'fig_paths':fig_paths})
     new_geo_df = gpd.GeoDataFrame(new_df, crs="EPSG:"+str(epsg), geometry=new_lines)
     
     new_geo_df.to_file(transects_path_name)